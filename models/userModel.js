9f7148167b03504b4df7ef233b869b54d9d5e698
priya shukla <priyashukla22@navgurukul.org>
2025-02-22 22:33:30 +0530

    updated file

---
+++ models/userModel.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6, // Minimum 6 characters
+  },
+  role: {
+    type: String,
+    enum: ["user", "admin"],
+    default: "user",
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+// Model create karna
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
